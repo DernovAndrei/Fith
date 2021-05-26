9531c925b98fbef6160b7f378ad7fa84b3698cd0
Andrei <dernovaw@gmail.com>
2021-05-25 23:10:13 -0500

    Initial Commit

---
+++ Fith/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Fith
+//
+//  Created by Andrey Dernov on 5/25/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
